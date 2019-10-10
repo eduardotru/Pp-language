95641f3ffb36652ee8e86df2a3d266d69c1fd8c6
Eduardo Trujillo <edytr1997@gmail.com>
2019-10-10 17:39:11 -0500

    Create example of functions with parameters

---
+++ Examples/functions_example.pp
@@ -0,0 +1,14 @@
+let string a, b, c;
+let int x, y, z;
+let bool t, f;
+
+func int f1(int a, int b, int c, int d) {
+  let int x, y, z;
+  return a;
+}
+
+func int f2(string a, int b, float c, bool d) {
+  let string x, y, z;
+  return a;
+}
+
