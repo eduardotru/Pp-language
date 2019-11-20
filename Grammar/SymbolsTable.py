6b93df5cbb7562b0153c952a2fc29c8efeebd10d
Huvok <h.garciaperez58@gmail.com>
2019-11-20 11:42:35 -0600

    Added starter pointers for type memory

--- Grammar/SymbolsTable.py
+++ Grammar/SymbolsTable.py
@@ -9,6 +9,7 @@ class BasicTypes(Enum):
     FLOAT = "float"
     VOID = "void"
 
+
 class StructuredTypes(Enum):
     NONE = "none"
     MATRIX = "matrix"
@@ -17,7 +18,7 @@ class StructuredTypes(Enum):
 
 
 class Type:
-    def __init__(self, basic_type, struct_type, rows = None, cols = None):
+    def __init__(self, basic_type, struct_type, rows=None, cols=None):
         self.basic_type = basic_type
         self.struct_type = struct_type
         self.rows = rows
@@ -42,7 +43,7 @@ class Function:
 
 
 class Variable:
-    def __init__(self, name, data_type, scope, memory_dir = 0):
+    def __init__(self, name, data_type, scope, memory_dir=0):
         self.name = name
         self.type = data_type
         self.scope = scope
@@ -55,6 +56,33 @@ class SymbolsTable:
         self.dir_to_memory_dict = {}
         self.functions = {}
         self.add_function("program", BasicTypes.VOID, {})
+        self.globVarInt = 0
+        self.globVarFloat = 1000
+        self.globVarBool = 2000
+        self.globVarString = 3000
+        self.globMatInt = 10000
+        self.globMatFloat = 11000
+        self.globMatBool = 12000
+        self.globMatString = 13000
+        self.constInt = 20000
+        self.constFloat = 21000
+        self.constBool = 22000
+        self.constString = 23000
+
+        # Just for reference, used in quadruples
+        self.globTempInt = 30000
+        self.globTempFloat = 31000
+        self.globTempBool = 32000
+        self.globTempString = 33000
+
+        self.locVarInt = 100000
+        self.locVarFloat = 101000
+        self.locVarBool = 102000
+        self.locVarSring = 103000
+        self.locMatInt = 110000
+        self.locMatFloat = 111000
+        self.locMatBool = 112000
+        self.locMatString = 113000
 
     def add_function(self, name, return_type, parameters):
         if name in self.functions:
@@ -80,12 +108,14 @@ class SymbolsTable:
         if name in self.functions[scope].variables:
             raise Exception("The variable already exists in this scope.")
         else:
-            self.functions[scope].variables[name] = Variable(
-                name, data_type, scope, self.memory_pointer)
-            self.dir_to_memory_dict[self.memory_pointer] = name
-            self.memory_pointer = self.memory_pointer + 1
-    
-
+            if (data_type.basic_type == BasicTypes.INT):
+                ptr = self.globVarInt
+                self.globVarInt += 1
+            else:
+                ptr = 0
+            var = Variable(name, data_type, scope, ptr)
+            self.functions[scope].variables[name] = var
+            self.dir_to_memory_dict[ptr] = name
 
     def exists_variable(self, name, scope):
         if name in self.functions[scope].variables or name in self.functions["program"].variables:
@@ -127,10 +157,11 @@ class SymbolsTable:
         return self.functions[name].parameters[index].type
 
     def set_function_memory(self, name, temp_memory):
-        self.functions[name].memory_size = temp_memory + len(self.functions[name].variables)
+        self.functions[name].memory_size = temp_memory + \
+            len(self.functions[name].variables)
 
     def get_function_memory(self, name):
-        return self.functions[name].memory_size 
+        return self.functions[name].memory_size
 
     def __str__(self):
         ret = "===============SYMBOLS=TABLE===============\n"
