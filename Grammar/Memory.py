1690b33bbf7217203799b7a547322ec1dfe25075
Eduardo Trujillo <edytr1997@gmail.com>
2019-11-22 18:58:07 -0600

    Create memory given a representation

--- Grammar/Memory.py
+++ Grammar/Memory.py
@@ -1,5 +1,21 @@
+import numpy as np
+
+from MemoryGenerator import MemoryRepresentation
+
 class Memory:
-    def __init__(self, ints = 0, floats = 0):
-        self.ints = [0 for _ in range(ints)]
-        self.floats = [0.0 for _ in range(floats)]
-        # And this for all data types
+    def __init__(self, memory_repr: MemoryRepresentation):
+        self.memory_repr = memory_repr
+        self.ints = [0 for _ in range(memory_repr.intRange[1] - memory_repr.intRange[0] + 1)]
+        self.floats = [0.0 for _ in range(memory_repr.intRange[1] - memory_repr.intRange[0] + 1)]
+        self.bools = [False for _ in range(memory_repr.intRange[1] - memory_repr.intRange[0] + 1)]
+        self.strings = ["" for _ in range(memory_repr.intRange[1] - memory_repr.intRange[0] + 1)]
+        self.matInts = [np.zeros((rows, cols), dtype=np.int64) for [rows, cols] in memory_repr.matInts]
+        self.matFloats = [np.zeros((rows, cols), dtype=np.float64) for [rows, cols] in memory_repr.matFloats]
+        self.matBools = [np.zeros((rows, cols), dtype=np.bool_) for [rows, cols] in memory_repr.matBools]
+        self.matStrings = [ [ ["" for _ in range(cols)] for _ in range(rows) ] for [rows, cols] in memory_repr.matStrings]
+
+    def get_value(self, address):
+        pass
+
+    def set_value(self, address):
+        pass
